55d12159612980f4e5b0006307022932e745ae84
Chinmay-163 <chinmay.sahoo@voltmoney.in>
2026-04-13 23:45:50 +0530

    Added Selenium_003 script to open YouTube video

---
+++ src/test/java/com/theTestingAcademy/Selenium_003.java
@@ -0,0 +1,11 @@
+package com.theTestingAcademy;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium_003 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.youtube.com/watch?v=PA-1_VlPCZc");
+    }
+}
