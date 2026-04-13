0780f5456f8e1ce19bd87b4a56715cb34da6bef9
Chinmay-163 <chinmay.sahoo@voltmoney.in>
2026-04-13 22:23:58 +0530

    Selenium_Code_Push

---
+++ src/test/java/com/theTestingAcademy/Selenium_002.java
@@ -0,0 +1,11 @@
+package com.theTestingAcademy;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium_002 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get(" https://openai.com/index/chatgpt/");
+    }
+}
